0d62fd3bcbb7dbef5978dbfe597b7a884da3efe5
e.ternovskiy <ternovskiy.egor555@gmail.com>
2023-10-05 16:19:31 +0300

    pr3 points update

--- pr3/points.c
+++ pr3/points.c
@@ -8,14 +8,10 @@ typedef struct {
     double mas;
 } Point;
 
-// sort point by mass
-void sortPoints(Point * points, int n);
 // consuming distance between two points
 double findDistance(Point p1, Point p2);
-// compare points by mass
-double comparePoints(Point p1, Point p2) {
-    return p1.mas - p2.mas;
-}
+// find point with min mas
+int findMinMasPointInd(Point * points, int n);
 // print all points
 void printPoints(Point * points, int n);
     
@@ -41,44 +37,55 @@ int main() {
 
     printf("Your points:\n");
     printPoints(&points[0], n);
+
     while (n > 1) {
-        int minIdx = 0;
-        double minDistance = findDistance(points[0], points[1]);
-        // find min distance
-        for (int i = 1; i < n - 1; i++) {
-            sortPoints(&points[0], n);
-            double dist = findDistance(points[0], points[i]);
-            if (dist < minDistance) {
-                minDistance = dist;
-                minIdx = i;
-            }
+        int idx = findMinMasPointInd(&points[0], n);
+        int nearestIndx = 0;
+        double minDist;
+        if (idx == 0) {
+            minDist = findDistance(points[idx], points[1]);
+            nearestIndx = 1;
+        } else {
+            minDist = findDistance(points[idx], points[0]);
         }
-        points[minIdx].mas += points[0].mas;
-        for (int z = 0; z < n - 1; z++) {
-            points[z] = points[z + 1];
+        for (int i = 1; i < n; i++) {
+            if (i != idx) {
+                double distance = findDistance(points[idx], points[i]);
+                if (distance < minDist) {
+                    nearestIndx = i;
+                    minDist = distance;
+                }
+            } 
+        }
+        points[nearestIndx].mas += points[idx].mas;
+        // delete element
+        for (int i = idx; i < n - 1; i++) {
+            points[i] = points[i + 1];
         }
         n--;
         points = (Point*)realloc(points, sizeof(Point) * n);
-        //printPoints(&points[0], n);
+        printPoints(&points[0], n);
     }
     printf("Last point(%.1lf; %.1lf) mas: %.1lf\n", points[n - 1].x, points[n - 1].y, points[n - 1].mas);
     return 0;
 }
 
 double findDistance(Point p1, Point p2) {
-    return sqrt(pow((p2.x - p1.x), 2) - pow((p2.y - p1.y), 2));
+    double a = pow(p2.x - p1.x, 2);
+    double b = pow(p2.y - p1.y, 2);
+    return sqrt( a + b );
 }
 
-void sortPoints(Point * points, int n) {
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (points[j].mas > points[j + 1].mas) {
-                Point tmp = points[j];
-                points[j] = points[j + 1];
-                points[j + 1] = tmp;
-            }
+int findMinMasPointInd(Point * points, int n) {
+    int idx = 0;
+    double minMas = points[0].mas;
+    for (int i = 1; i < n; i++) {
+        if (minMas > points[i].mas) {
+            minMas = points[i].mas;
+            idx = i;
         }
     }
+    return idx;
 }
 
 void printPoints(Point * points, int n) {
